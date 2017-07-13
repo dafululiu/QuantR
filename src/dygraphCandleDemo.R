0474767abcbf084987455ecf65f74d8ad7bd79e7
zhichaoluo <zhichao.luo@gmail.com>
2017-07-13 15:41:44 +0800

    add event

--- src/dygraphCandleDemo.R
+++ src/dygraphCandleDemo.R
@@ -15,13 +15,22 @@ ds$ma5<-rollmean(ds$CLOSE,5,fill = NA,
                     align = 'right')
 ds$ma20<-rollmean(ds$CLOSE,20,fill = NA,
                  align = 'right')
-
+ds$lagClose<-c(NA,ds$CLOSE[-length(ds$CLOSE)])
+ds$ret<-(ds$CLOSE-ds$lagClose)/ds$lagClose
+ds$signal<-sign(ds$ma5-ds$ma20)
+ds$s2<-c(NA,diff(ds$signal))
+ds$s2<-ifelse(ds$s2==-2,"B",ifelse(ds$s2==2,"S",NA))
 ds<-as.xts(ds)
-class(ds)
 
-p<-dygraph(ds) %>%
+p<-dygraph(ds[,1:6]) %>%
   dyCandlestick() %>%
   dyRangeSelector(height = 10,
                   strokeColor = "")
+
+s<-index(ds[ds$s2=="S",])
+b<-index(ds[ds$s2=="B",])
+
+p <- p %>% dyEvent(c(s), c(rep('Sale',length(s))), labelLoc='bottom')
+p <- p %>% dyEvent( c(b), c(rep('Buy',length(b))), labelLoc='bottom')
 p
 
